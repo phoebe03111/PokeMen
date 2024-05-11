36d20c6b83232c3c722072e8b714a63463d82062
Phoebe Chang <phoebe03111@gmail.com>
2024-05-11 10:41:28 -0700

    Create header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import logo from "../../public/logo.png";
+
+const Header = () => {
+  return (
+    <header className="container mx-auto p-8 flex justify-between items-center">
+      <div className="flex items-center gap-2">
+        <img src={logo} alt="logo" width={40} />
+        <span className="font-bold text-2xl">Pokemon</span>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <Link to="/become-a-tutor">Become a Tutor</Link>
+        <Link to="/login">Log In</Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
